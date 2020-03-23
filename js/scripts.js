07678d563c8bea267ae12445777b3bd18838e78f
Leonardo Welter <leonardowelter@email.com>
2020-03-23 16:41:39 -0300

    Versão 0.0.1 - Mockup inicial da interface.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+function recuperarRespostas() {
+    let numQuestoes = document.querySelectorAll('input[type="radio"]').length / 5;
+    let respostas = [];
+
+    let i = 0;
+
+    while (i < numQuestoes) {
+        respostas[document.querySelectorAll('input[type="radio"]:checked')[i].name] = document.querySelectorAll('input[type="radio"]:checked')[i].value;
+        i++
+    }
+
+    return respostas;
+}
+
+
+function recuperarRespostas2() {
+    let numQuestoes = document.querySelectorAll('input[type="radio"]').length / 5;
+    let respostas = [];
+
+    let i = 0;
+
+    while (i < numQuestoes) {
+        respostas.push({ [document.querySelectorAll('input[type="radio"]:checked')[i].name] : document.querySelectorAll('input[type="radio"]:checked')[i].value });
+        i++
+    }
+
+    return JSON.stringify(respostas);
+}
